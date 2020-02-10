c0b2870386ae815eb039315fbaf0b87991575698
juan solano <juanpablosolanosuarez@hotmail.com>
2020-02-10 15:46:44 +0100

    PersistentBugger

---
+++ Code_Wars_persistentBugger.js
@@ -0,0 +1,17 @@
+
+function persistence(num) {
+  let number = num
+  let finalnumber = 1
+  let count_array = []
+
+  while(number.toString().split("").length >= 2){
+    number.toString().split("").forEach(element =>{
+      finalnumber *= element
+    })
+    number = finalnumber
+    finalnumber = 1
+    count_array.push(number)
+  }
+  return(count_array.length)
+}
+console.log(persistence(999))
