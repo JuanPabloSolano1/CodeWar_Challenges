4c8954426acd59e83b844bc5e6f3eac7e14f2390
juan solano <juanpablosolanosuarez@hotmail.com>
2020-05-13 18:00:34 +0200

    added new example

--- Extending_Javascript_Objects_Reverse_string.js
+++ Extending_Javascript_Objects_Reverse_string.js
@@ -1,13 +1,21 @@
 
 String.prototype.reversed = function() {
-  let string = this.split("")
-  let count = string.length - 1
+  let count = this.length - 1
   let new_string = ""
   while (count >= 0){
-    new_string += (string[count])
+    new_string += (this[count])
     count --
   }
   return(new_string)
 }
 
+console.log("Hello There".reversed())
+
+String.prototype.reversed = function() {
+  let new_array = []
+  for(i = this.length; i >= 0 ; i--){
+    new_array.push(this[i])
+  }
+  return new_array.join("")
+}
 console.log("Hello There".reversed())
